b5ca67d89f67300dba79c6d4e58a60ad631aea86
Scott <sbelan@uwo.ca>
2016-12-19 18:55:07 -0500

    Changed file extensions for express serving webpages Fixed some spacing issues on services page

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
-
-app.use(express.static('./public/'));
+app.use(express.static(__dirname + '/public', {
+  extensions: ['html']
+}));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000')
