60d9a7c610c826735d4ff51f1dc9172e7100fcd2
Scott <sbelan@uwo.ca>
2016-12-19 15:00:14 -0500

    Updated viewports for more responsive layout

--- public/js/navbarHandler.js
+++ public/js/navbarHandler.js
@@ -4,6 +4,7 @@
     $('#navbarSolid').fadeOut();
     $(function () {
         $(window).scroll(function () {
+        console.log("working");
             var scroll = $(window).scrollTop();
             
             if (scroll > 600) {
